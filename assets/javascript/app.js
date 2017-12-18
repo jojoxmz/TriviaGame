1748536522b75e68f71ec2ab628c78c7122e6b1e
JoJo Zheng^C <jojozheng1216@hotmail.com>
2017-12-18 15:00:30 -0700

    added new details

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
-console.log('Hey!')
+//counter
+var correct= 0;
+var incorrect= 0;
+var unanswered= 0;
+
+var startButton= true;
+
+var timeRemaining
+
+
+//when start button is clicked- game starts
+
+
+
