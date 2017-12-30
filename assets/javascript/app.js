d7d6746227d173229f26b96057412d3ad2f4a00c
JoJo Zheng^C <jojozheng1216@hotmail.com>
2017-12-30 01:01:34 -0700

    not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,40 +3,40 @@ var correct= 0;
 var incorrect= 0;
 var unanswered= 0;
 
-var questions= [
+var allQuestions= [
 	{
 		question: "Through what part of the body do dogs sweat?",
-		correct: "Paws",
-		answer: ["Mouth", "Ears", "Nose", "Paws"],
+		correctAnswer: 3,
+		allAnswers: ["Mouth", "Ears", "Nose", "Paws"],
 	},
 	{
 		question: "Is Ben the best?",
-		correct: "Abso-freakin-lootely",
-		answers: ["Nope he's a dumb butt","Who's Ben?", "Abso-freakin-lootely", "Apple pie"]
+		correctAnswer: 2,
+		allAnswers: ["Nope he's a dumb butt","Who's Ben?", "Abso-freakin-lootely", "Apple pie"]
 	},
 
 	{
 		question: "Which TV series had a dog named K9 who was also a robot?",
-		correct: "Doctor Who",
-		answers:["Full House", "Doctor Who", "Star Trek", "Law & Order"]
+		correctAnswer: 1,
+		allAnswers:["Full House", "Doctor Who", "Star Trek", "Law & Order"]
 	},
 
 	{
 		question: "What breed of dog is the smallest used in hunting?",
-		correct: "Miniature dachshund",
-		answers:["Miniature dachshund", "Chihuahua", "Toy poodle", "Smooth fox terrier"],
+		correctAnswer: 0,
+		allAnswers:["Miniature dachshund", "Chihuahua", "Toy poodle", "Smooth fox terrier"],
 	},
 
 	{
 		question: "Which dog breed has a black tongue?",
-		correct: "Chow Chow",
-		answers:["Husky", "Labrador", "Chow Chow", "Weimaraner"],
+		correctAnswer: 2,
+		allAnswers:["Husky", "Labrador", "Chow Chow", "Weimaraner"],
 	},
 
 	{
 		question: "Which breed was once known as St. John’s Newfoundland?",
-		correct: "Labrador",
-		answers:["Labrador", "Newfoundland", "Golden retriever", "Puli"],
+		correctAnswer: 0,
+		allAnswers:["Labrador", "Newfoundland", "Golden retriever", "Puli"],
 	}	
 ];
 
@@ -93,70 +93,111 @@ function timeConverter(t) {
 
 
 // var questionsContainer= $('#quiz');
-var results= $('#results');
-var submitButton= $('#submit-button');
-var userAnswer = '';
+// var results= $('#results');
+// var submitButton= $('#submit-button');
+// var userAnswer = '';
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	$('#questions').hide();	
+// 	$('#questions').hide();	
 
-		//when start button is clicked- game starts
-		 $("#start-button").on("click", function() {
-		   	console.log('start button clicked')
-		    $('#start-button').remove();
+// 		//when start button is clicked- game starts
+// 		 $("#start-button").on("click", function() {
+// 		   	console.log('start button clicked')
+// 		    $('#start-button').remove();
 			
-			$("#timer").text("Time Remaining: 00:00");
-	    	start();
+// 			$("#timer").text("Time Remaining: 00:00");
+// 	    	start();
 
-			$('#questions').show();
+// 			$('#questions').show();
 
-			// userAnswer = $('.correctAnswer:input:checked')||{}.value;
+// 			// userAnswer = $('.correctAnswer:input:checked')||{}.value;
             
-            // if answer is correct
-            if(userAnswer === ($('.correctAnswer:input:checked'))){
-                // add to the number of correct answers
-                correct ++;
-                console.log('This is: ' + correct)
-                };
+//             // if answer is correct
+//             if(userAnswer === ($('.correctAnswer:input:checked'))){
+//                 // add to the number of correct answers
+//                 correct ++;
+//                 console.log('This is: ' + correct)
+//                 };
 
-		 });
+// 		 });
 
-		 if (time === 0) {
-		 	$(results).text('Correct: ' + correct + ' Incorrect: ' + incorrect + ' Unanswered: ' + unanswered);
-		 	console.log('Correct: ' + correct + ' Incorrect: ' + incorrect + ' Unanswered: ' + unanswered);
+// 		 if (time === 0) {
+// 		 	$(results).text('Correct: ' + correct + ' Incorrect: ' + incorrect + ' Unanswered: ' + unanswered);
+// 		 	console.log('Correct: ' + correct + ' Incorrect: ' + incorrect + ' Unanswered: ' + unanswered);
 
-		 };
+// 		 };
 
 	
 			
+// var currentquestion = 0;
+var correctAnswers = 0;
+var incorrectAnswers= 0;
 
+function setupOptions() {
+  $('#questions').html(allQuestions.question);
+  var options = allQuestions.allAnswers;
+  var formHtml 
+  // for (var i = 0; i < options.length; i++) {
+    formHtml = '<div><input type="radio" name="allAnswers" value="' + '" id="allAnswers' + '"><label for="allAnswers' + '">' +
+      allQuestions.allAnswers + '</label></div><br/>';
+  // }
+  $('#questionsForm').html(formHtml);
+  $("#options").prop('checked', true);
+};
 
+function checkAnswers() {
+  if ($("input[name=allAnswers]:checked").val() === allQuestions.correctAnswer) {
+    correctAnswers++;
+  } else {
+  	incorrectAnswers++;
+  };
+};
 
+$(document).ready(function() {
+	$('#questions').hide();	
+  
+  $('#start-button').click(function() {
+    $(this).hide();
 
+	$("#timer").text("Time Remaining: 00:00");
+	start();
+	$('#questions').show();
+	setupOptions();
+	checkAnswers();
+  });
 
-		 // 		//should be used on submit button
-			// var correctAnswerChoice= false;
+  // $(function() {
+  //   $("#progressbar").progressbar({
+  //     max: allQuestions.length - 1,
+  //     value: 0
+  //   });
+  });
 
-		 // 	$('.correctAnswer').click(function(){
+  
 
-		 // 		correctAnswerChoice= true;
-		 // 		correct ++;
-		 // 		console.log('correct: ' + correct);	
-		 //  	});
+  // $("#next").click(function() {
+    // event.preventDefault();
+    
+    // currentquestion++;
+    // $(function() {
+    //   $("#progressbar").progressbar({
+    //     value: currentquestion
+    //   });
+    // });
+    // if (currentquestion < allQuestions.length) {
+    //   setupOptions();
+      if (time === 0) {
+        // $('#next').html("Submit");
+        $('#submit-button').click(function() {
+          $("#questions").hide();
+          $("#results").html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").hide();
+          $("#results").fadeIn(1500);
+        });
 
-		 // 	$('.notCorrect').click(function(){
+      };
 
-		 // 		correctAnswerChoice;
-		 // 		incorrect ++;
-		 // 		console.log('incorrect: ' + correct);	
-		 //  	}); 
-
-
-
-			
-
-
-})
-
+    // };
+//   });
+// });
